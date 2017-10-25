c5aedb4f1205c737dd016856cde2e3fdd7bc53c8
sts <escs34@gmail.com>
2017-10-25 04:13:51 +0000

    save

---
+++ proj3/another.py
@@ -0,0 +1,11 @@
+import test2
+import RPi.GPIO as GPIO
+from time import sleep
+
+a=test2.t()
+a.p()
+a.q()
+sleep(1)
+a.r()
+
+
--- proj3/test.py
+++ proj3/test.py
@@ -5,6 +5,8 @@ from time import sleep
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
+forward0=True
+
 #const variable	
 MOTOR_LEFT_A = 12
 MOTOR_LEFT_B = 11
@@ -26,8 +28,27 @@ GPIO.setup(MOTOR_RIGHT_PWM, GPIO.OUT)
 
 LeftPwm=GPIO.PWM(MOTOR_LEFT_PWM, 100)#create PWM
 RightPwm=GPIO.PWM(MOTOR_RIGHT_PWM, 100)
+#
 
-#public method
+	
+def p():
+	GPIO.output(MOTOR_LEFT_A, GPIO.LOW)
+	GPIO.output(MOTOR_LEFT_B, GPIO.HIGH)
+
+def q():
+	GPIO.output(MOTOR_LEFT_PWM, GPIO.HIGH)
+
+def r():
+	GPIO.output(MOTOR_LEFT_PWM, GPIO.LOW)
+
+p()
+q()
+sleep(1)
+r()
+print("end")
+
+#
+'''#public method
 def go(leftDirection, rightDirection, leftSpeed, rightSpeed, duration):
 	setDirection(leftDirection, rightDirection)#set direction
 	setSpeed(leftSpeed, rightSpeed)#set speed
@@ -47,9 +68,9 @@ def setSpeed(leftSpeed, rightSpeed):
 	RightPwm.ChangeDutyCycle(rightSpeed)
 		
 
-A.go(True, True, 40, 40, 3)
-A.go(True, True, 0, 0, 1)
-
+go(True, True, 40, 40, 3)
+go(True, True, 0, 0, 1)
+'''
 
 
 
