d8d82b1772a9fb048ef1d9f25fe1ca9ac129ad67
sts <escs34@gmail.com>
2017-10-25 12:33:52 +0900

    pass4

--- proj3/test.py
+++ proj3/test.py
@@ -1,63 +1,54 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-class PPicar:
-       	GPIO.setmode(GPIO.BOARD)
-	GPIO.setwarnings(False)
 
-	#const variable	
-	MOTOR_LEFT_A = 12
-	MOTOR_LEFT_B = 11
-	MOTOR_LEFT_PWM = 35
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
 
-	MOTOR_RIGHT_A = 15
-	MOTOR_RIGHT_B = 13
-	MOTOR_RIGHT_PWM = 37
+#const variable	
+MOTOR_LEFT_A = 12
+MOTOR_LEFT_B = 11
+MOTOR_LEFT_PWM = 35
+
+MOTOR_RIGHT_A = 15
+MOTOR_RIGHT_B = 13
+MOTOR_RIGHT_PWM = 37
 
 
-	#base setting
-	GPIO.setup(MOTOR_LEFT_A, GPIO.OUT)#set left motor pin to output
-	GPIO.setup(MOTOR_LEFT_B, GPIO.OUT)
-	GPIO.setup(MOTOR_LEFT_PWM, GPIO.OUT)
+#base setting
+GPIO.setup(MOTOR_LEFT_A, GPIO.OUT)#set left motor pin to output
+GPIO.setup(MOTOR_LEFT_B, GPIO.OUT)
+GPIO.setup(MOTOR_LEFT_PWM, GPIO.OUT)
 
-	GPIO.setup(MOTOR_RIGHT_A, GPIO.OUT)#set right moto pin to output
-	GPIO.setup(MOTOR_RIGHT_B, GPIO.OUT)
-	GPIO.setup(MOTOR_RIGHT_PWM, GPIO.OUT)
+GPIO.setup(MOTOR_RIGHT_A, GPIO.OUT)#set right moto pin to output
+GPIO.setup(MOTOR_RIGHT_B, GPIO.OUT)
+GPIO.setup(MOTOR_RIGHT_PWM, GPIO.OUT)
 
-	LeftPwm=GPIO.PWM(MOTOR_LEFT_PWM, 100)#create PWM
-	RightPwm=GPIO.PWM(MOTOR_RIGHT_PWM, 100)
-	
+LeftPwm=GPIO.PWM(MOTOR_LEFT_PWM, 100)#create PWM
+RightPwm=GPIO.PWM(MOTOR_RIGHT_PWM, 100)
 
-	def __init__(self):
-		print("PPika!")
-
-                #public method
-	def go(self, leftDirection, rightDirection, leftSpeed, rightSpeed, duration):
-		self.setDirection(leftDirection, rightDirection)#set direction
-		self.setSpeed(leftSpeed, rightSpeed)#set speed
-		sleep(duration)#set duration time
-                print("here")
+#public method
+def go(leftDirection, rightDirection, leftSpeed, rightSpeed, duration):
+	setDirection(leftDirection, rightDirection)#set direction
+	setSpeed(leftSpeed, rightSpeed)#set speed
+	sleep(duration)#set duration time
+        print("here")
 		
-	#private method
-	def REVERSE(self,x):
-		return not x
+#private method
+def setDirection(leftDirection, rightDirection):
+	GPIO.output(MOTOR_LEFT_A, GPIO.HIGH)#not leftDirection)#left motor
+	GPIO.output(MOTOR_LEFT_B, GPIO.LOW)
 
-	def setDirection(self, leftDirection, rightDirection):
-		GPIO.output(self.MOTOR_LEFT_A, GPIO.HIGH)#not leftDirection)#left motor
-		GPIO.output(self.MOTOR_LEFT_B, GPIO.LOW)
+	GPIO.output(MOTOR_RIGHT_A, rightDirection)#right motor
+	GPIO.output(MOTOR_RIGHT_B, not rightDirection)
 
-		GPIO.output(self.MOTOR_RIGHT_A, rightDirection)#right motor
-		GPIO.output(self.MOTOR_RIGHT_B, not rightDirection)
-
-	def setSpeed(self, leftSpeed, rightSpeed):
-		self.LeftPwm.ChangeDutyCycle(leftSpeed)
-		self.RightPwm.ChangeDutyCycle(rightSpeed)
+def setSpeed(leftSpeed, rightSpeed):
+	LeftPwm.ChangeDutyCycle(leftSpeed)
+	RightPwm.ChangeDutyCycle(rightSpeed)
 		
 
-if __name__ == "__main__":
-        A=PPicar()
-	A.go(True, True, 40, 40, 3)
-        A.go(True, True, 0, 0, 1)
+A.go(True, True, 40, 40, 3)
+A.go(True, True, 0, 0, 1)
 
 
 
